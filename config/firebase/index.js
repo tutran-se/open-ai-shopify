6ec6ca8633bf983dbb7f8af681c33f9b147356b3
Tu Tran <trandinhtu.dev@gmail.com>
2022-05-14 20:15:25 -0400

    Firebase Setup

---
+++ config/firebase/index.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export auth object
+export const auth = getAuth(app);
+
+// Export db (firestore) object
+export const db = getFirestore(app);
+
+// Export storage object
+export const storage = getStorage(app);
